6ac59870e6d1ee3428d12450bdfe7904bd60ce57
Brecht Vanden Houwe <brechtvandenhouwe@movetex.be>
2025-05-20 18:17:17 +0200

    added the pattern template

---
+++ DesignPatterns/PatternTemplate.cs
@@ -0,0 +1,55 @@
+﻿namespace DesignPatterns
+{
+    /// <summary>
+    /// Pattern Template
+    /// 
+    /// This is a template on how to structure the design pattern classes, first of start be explaining what this pattern is about,
+    /// then list the benefits of using this pattern.
+    /// 
+    /// Benefits:
+    /// - Having a template will improve the consistency and readability of the examples.
+    /// </summary>
+
+    #region Definitions
+
+    /// <summary>
+    /// First, define the enums, used in the example if any are used
+    /// </summary>
+    enum Patterns
+    {
+        Factory
+    }
+
+    /// <summary>
+    /// Second, define the interfaces used in the example
+    /// </summary>
+    interface IPatternTemplate
+    {
+
+    }
+
+    #endregion
+
+    #region Design Pattern
+    /// <summary>
+    /// Third, define the concrete classes used in the example
+    /// </summary>
+    class PatternTemplate
+    {
+        
+    }
+    #endregion
+
+    #region Example
+    /// <summary>
+    /// The last class should be an exmaple on the pattern, to run the example, create a static function called RunExample
+    /// </summary>
+    class Pattern()
+    {
+        public static void RunExample()
+        {
+            Console.WriteLine("This is a template for a design pattern example.");
+        }
+    }
+    #endregion
+}
